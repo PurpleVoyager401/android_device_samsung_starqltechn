075a9876e4753627bfa1c37a80574c5e8e6c3587
! Just Dan ! <flugel-sama@protonmail.com>
2024-10-10 19:02:47 +0000

    starqltechn: Drop libinit
    Target properties added into lineage_starqltechn.mk

--- lineage_starqltechn.mk
+++ lineage_starqltechn.mk
@@ -31,4 +31,15 @@ PRODUCT_DEVICE := starqltechn
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G9600
 PRODUCT_MANUFACTURER := samsung
+
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+BUILD_FINGERPRINT := "samsung/starqltezh/starqltechn:10/QP1A.190711.020/G9600ZHU9FWC3:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=starqltezh \
+    PRIVATE_BUILD_DESC="starqltezh-user 10 QP1A.190711.020 G9600ZHU9FWC3 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.PDA=G9600ZHU9FWC3 \
+    ro.build.fingerprint=samsung/starqltezh/starqltechn:10/QP1A.190711.020/G9600ZHU9FWC3:user/release-keys
